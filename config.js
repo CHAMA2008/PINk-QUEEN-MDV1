66a61a6d52bcf094a9ff1fdbb2a605e89a8493ca
CHAMA2008 <chamamdmax2008@gmail.com>
2025-05-04 17:31:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CHAMA-MD=jQoFALqJ#gzT09kmPYzwq5wdXa9ydRE9K4Z1XidalouZZnHEo0M0",
+SESSION_ID: process.env.SESSION_ID || "CHAMA-MD=vFonkTDa#j5qEGHeg8-gl8ruqyRKcN2ckH2NjGauMcXjw7vMK2OI",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
